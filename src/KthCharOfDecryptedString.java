c8dabd01b1c7e4f2c550b7c50c6e1154203891a4
Robin Kamboj <dev.robinkamboj@gmail.com>
2018-01-22 23:26:54 +0530

    initial commit

---
+++ src/KthCharOfDecryptedString.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class KthCharOfDecryptedString {
+
+    //encrypted input: a12b3, k = 5
+    //decrypted string: aaaaaaaaaaaabbb
+    //output: a
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String input = in.next();
+        int k = in.nextInt();
+        int index = 0;
+    }
+}
---
+++ src/LongestSpanSameSumTwoBinaryArrays.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class LongestSpanSameSumTwoBinaryArrays {
+
+    static int[] a, b;
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int size = in.nextInt();
+
+        a = new int[size];
+        b = new int[size];
+
+        for(int i=0; i<a.length; i++){
+            a[i] = in.nextInt();
+        }
+
+        for(int i=0; i<b.length; i++){
+            b[i] = in.nextInt();
+        }
+
+        //Naive solution (Complexity: O(n^2)
+        int longest_span = 0;
+        int span;
+        for(int i=0; i<size; i++){
+            if(a[i] == b[i]){
+                span = 1;
+            }
+            else{
+                continue;
+            }
+            int j = i+1;
+            while (j<size && a[j] == b[j]){
+                span++;
+                j++;
+            }
+            if(longest_span < span) longest_span = span;
+        }
+
+        System.out.println("Longest span is: " + longest_span);
+    }
+}
