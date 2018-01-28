a67357ee19fe7bb3f541959bba179b0281385866
Robin Kamboj <dev.robinkamboj@gmail.com>
2018-01-28 17:27:49 +0530

    linked list using custom nodes java

---
+++ src/linked_list_java/Node.java
@@ -0,0 +1,20 @@
+package linked_list_java;
+
+public class Node {
+
+    private int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+        next = null;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+}
