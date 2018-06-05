9ef2b6775deb24f0414207a6bd27eef8d52877b1
Robin Kamboj <dev.robinkamboj@gmail.com>
2018-06-06 03:54:15 +0530

    more recursion questions

---
+++ src/recursion_java/FirstIndex.java
@@ -0,0 +1,28 @@
+package recursion_java;
+
+import java.util.Scanner;
+
+public class FirstIndex {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] a = new int[n];
+        for(int i=0; i<n; i++) {
+            a[i] = in.nextInt();
+        }
+        int x = in.nextInt();
+
+        System.out.println(firstIndex(a, x));
+    }
+
+    public static int firstIndex(int input[], int x) {
+        return firstIndexFind(input, x, 0);
+    }
+
+    public static int firstIndexFind(int[] a, int x, int n) {
+        if(n == a.length) return -1;
+        if(a[n] == x) return n;
+        return firstIndexFind(a, x, n+1);
+    }
+}
