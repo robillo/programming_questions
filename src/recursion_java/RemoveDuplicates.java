b7631dccd3ef4175ac2ea13584b698d719443c7d
Robin Kamboj <dev.robinkamboj@gmail.com>
2018-06-06 20:40:32 +0530

    staircase (important) + more

---
+++ src/recursion_java/RemoveDuplicates.java
@@ -0,0 +1,23 @@
+package recursion_java;
+
+import java.util.Scanner;
+
+public class RemoveDuplicates {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.nextLine();
+
+        System.out.println(removeConsecutiveDuplicates(s));
+    }
+
+    public static String removeConsecutiveDuplicates(String s) {
+        return removeDups(s);
+    }
+
+    public static String removeDups(String s) {
+        if ( s.length() <= 1 ) return s;
+        if( s.substring(1,2).equals(s.substring(0,1)) ) return removeDups(s.substring(1));
+        else return s.substring(0,1) + removeDups(s.substring(1));
+    }
+}
