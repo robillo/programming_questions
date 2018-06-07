8c5ad009ee6976147abece2d29bcdff6658f3498
Robin Kamboj <dev.robinkamboj@gmail.com>
2018-06-07 18:42:55 +0530

    important: interleavings and minimum count

--- src/recursion_java/InterLeavings.java
+++ src/recursion_java/InterLeavings.java
@@ -13,8 +13,21 @@ public class InterLeavings {
     }
 
     public static void interleavings(String first, String second){
+        printInterleavings(first, second, "");
+    }
 
-        // Write your code here
-
+    public static void printInterleavings(String s1, String s2, String soFar){
+        if((s1==null||s1.length()==0) && (s2==null||s2.length() == 0))
+            return;
+        if(s1==null || s1.length()==0){
+            System.out.println(soFar + s2);
+            return;
+        }
+        if(s2==null || s2.length()==0){
+            System.out.println(soFar + s1);
+            return;
+        }
+        printInterleavings(s1.substring(1), s2, soFar + s1.charAt(0));
+        printInterleavings(s1, s2.substring(1), soFar + s2.charAt(0));
     }
 }
