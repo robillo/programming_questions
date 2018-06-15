333c7e4cc57377f6cf982c57e9aefa1a65dc90a7
Robin Kamboj <dev.robinkamboj@gmail.com>
2018-06-15 10:59:20 +0530

    return keypad strings problem done. awesome feeling.

--- src/recursion_java/ReturnKeypadStrings.java
+++ src/recursion_java/ReturnKeypadStrings.java
@@ -1,5 +1,6 @@
 package recursion_java;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -7,23 +8,63 @@ public class ReturnKeypadStrings {
 
     static HashMap<Integer, String> map = new HashMap<>();
 
+    static {
+        map.put(2, "abc");
+        map.put(3, "def");
+        map.put(4, "ghi");
+        map.put(5, "jkl");
+        map.put(6, "mno");
+        map.put(7, "pqrs");
+        map.put(8, "tuv");
+        map.put(9, "wxyz");
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
 
-        map.put(1, "abc");
-        map.put(2, "def");
-        map.put(3, "ghi");
-        map.put(4, "jkl");
-        map.put(5, "mno");
-        map.put(6, "pqrs");
-        map.put(7, "tuv");
-        map.put(8, "wxyz");
+        map.put(2, "abc");
+        map.put(3, "def");
+        map.put(4, "ghi");
+        map.put(5, "jkl");
+        map.put(6, "mno");
+        map.put(7, "pqrs");
+        map.put(8, "tuv");
+        map.put(9, "wxyz");
 
-        keypad(n);
+        System.out.println(Arrays.toString(keypad(n)));
     }
 
     public static String[] keypad(int n){
-        return null;
+        if(n == 0) return null;
+        int currentNum = n%10;
+        n = n/10;
+        String mappedString = map.get(currentNum);
+        String[] recur = keypad(n);
+        int size;
+        if(recur == null) {
+            size = mappedString.length();
+        }
+        else {
+            size = mappedString.length() * recur.length;
+        }
+        String[] updatedStrings = new String[size];
+        if(recur == null) {
+            for(int i=0; i<mappedString.length(); i++) {
+                updatedStrings[i] = mappedString.charAt(i) + "";
+            }
+        }
+        else {
+            int count = 0;
+            for(int i=0; i<recur.length; i++) {
+                String temp = recur[i];
+                for (char item : mappedString.toCharArray()) {
+                    updatedStrings[count] = temp + (item + "");
+                    count++;
+                }
+            }
+        }
+        return updatedStrings;
     }
+
 }
