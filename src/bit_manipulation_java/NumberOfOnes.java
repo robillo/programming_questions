48050cb7f4ec662e11542bf564e556406249e6ac
Robin Kamboj <dev.robinkamboj@gmail.com>
2018-07-01 02:36:24 +0530

    find number of ones in binary representation of a number

---
+++ src/bit_manipulation_java/NumberOfOnes.java
@@ -0,0 +1,22 @@
+package bit_manipulation_java;
+
+import java.util.Scanner;
+
+public class NumberOfOnes {
+
+        /*You are given an integer N.
+    You need to count number of 1's in its binary representation*/
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        int count = 0;
+        while (n > 0) {
+            n = n & n-1;
+            count++;
+        }
+
+        System.out.println(count);
+    }
+}
