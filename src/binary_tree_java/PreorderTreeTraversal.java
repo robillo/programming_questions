b03f5643a586f52df7cf9b23fa8ce11bac492392
Robin Kamboj <dev.robinkamboj@gmail.com>
2018-07-07 00:34:32 +0530

    longest increasing subsequence dp

--- src/binary_tree_java/PreorderTreeTraversal.java
+++ src/binary_tree_java/PreorderTreeTraversal.java
@@ -1,5 +1,9 @@
 package binary_tree_java;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class PreorderTreeTraversal {
 
     /* Traversal will be (left right root) : 1 2 4 6 7 3
@@ -15,7 +19,31 @@ public class PreorderTreeTraversal {
     public static void main(String[] args) {
         BinaryTree tree = BinaryTree.inflateSampleTree();
 
-        traversePreorder(tree.root);
+        List<Integer> list = new ArrayList<>();
+
+        preOrderAllPaths(tree.root, list, 0);
+
+//        traversePreorder(tree.root);
+    }
+
+    private static void preOrderAllPaths(Node root, List<Integer> a, int pos) {
+        if(root == null) return;
+
+        a.add(pos, root.getValue());
+
+        if(root.left == null && root.right == null) {
+            int count = 0;
+            for(Object item : a.toArray()) {
+                System.out.print(item + " ");
+                count++;
+                if(count > pos) break;
+            }
+            System.out.println();
+            return;
+        }
+
+        preOrderAllPaths(root.left, a, pos+1);
+        preOrderAllPaths(root.right, a, pos+1);
     }
 
     private static void traversePreorder(Node root) {
