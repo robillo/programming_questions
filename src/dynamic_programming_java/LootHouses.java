988bef5f08510461f4d35f9e6f23007ea4406e14
Robin Kamboj <dev.robinkamboj@gmail.com>
2018-07-08 03:17:16 +0530

    Loot Houses ideal DP solution

--- src/dynamic_programming_java/LootHouses.java
+++ src/dynamic_programming_java/LootHouses.java
@@ -30,6 +30,9 @@ public class LootHouses {
             sumFromLast[i]+=arr[i];
 
         int sum = 0;
+
+        if(n == 0) return arr[0];
+
         for(int i=0; i<n-2; i+=2) {
             if(sumFromLast[i] > sumFromLast[i+1]) sum+=sumFromLast[i];
             else {
@@ -39,5 +42,28 @@ public class LootHouses {
         }
 
         return sum;
+    }
+
+    public static int maxLoot(int hval[], int n) {
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return hval[0];
+        if (n == 2)
+            return Math.max(hval[0], hval[1]);
+
+        // dp[i] represent the maximum value stolen
+        // so far after reaching house i.
+        int[] dp = new int[n];
+
+        // Initialize the dp[0] and dp[1]
+        dp[0] = hval[0];
+        dp[1] = Math.max(hval[0], hval[1]);
+
+        // Fill remaining positions
+        for (int i = 2; i<n; i++)
+            dp[i] = Math.max(hval[i] + dp[i-2], dp[i-1]);
+
+        return dp[n-1];
     }
 }
