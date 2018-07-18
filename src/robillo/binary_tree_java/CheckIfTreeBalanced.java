f0ee465df11ced429ed0c642bfb1e3d5535fff36
Robin Kamboj <dev.robinkamboj@gmail.com>
2018-07-18 14:04:46 +0530

    balanced

---
+++ src/robillo/binary_tree_java/CheckIfTreeBalanced.java
@@ -0,0 +1,27 @@
+package robillo.binary_tree_java;
+
+import java.util.Scanner;
+
+public class CheckIfTreeBalanced {
+
+    public static void main(String[] args) {
+        BinaryTree tree = BinaryTree.inflateSampleTree();
+
+        int diff = checkDiff(tree.root, 0);
+
+        if(diff <= 1) System.out.println("TRUE");
+        else System.out.println("FALSE");
+    }
+
+    private static int checkDiff(Node root, int level) {
+        if(root == null) return level;
+        if(root.left == null && root.right == null) return level;
+
+        int a = checkDiff(root.left, level+1);
+        int b = checkDiff(root.right, level+1);
+
+        if(Math.abs(a-b) > 1) return Integer.MAX_VALUE;
+
+        return Math.abs(a-b);
+    }
+}
