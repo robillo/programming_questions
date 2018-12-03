760f4a6a83529e5aea2a5c91684ba2eec73f3044
Robin Kamboj <dev.robinkamboj@gmail.com>
2018-12-04 05:14:53 +0530

    selection sort

--- src/robillo/sorting/SelectionSort.java
+++ src/robillo/sorting/SelectionSort.java
@@ -4,20 +4,28 @@ public class SelectionSort {
 
     public static void main(String[] args) {
         int array[] = {64, 34, 25, 12, 22, 11, 90};
-        bubbleSort(array);
+        selectionSort(array);
         System.out.println("Sorted array is:");
         printArray(array);
     }
 
-    private static void bubbleSort(int[] array) {
+    private static void selectionSort(int[] array) {
         int n = array.length;
-        for(int i=0; i<n-1 ; i++) {         //leaving the last one, which will sort by itself
-            for(int j=0; j<n-i-1; j++) {    //first it ends at second last, then third last, ...
-                //swap
-                int temp = array[j];
-                array[j] = array[i];
-                array[i] = temp;
-            }
+
+        // One by one move boundary of unsorted subarray
+        for (int i = 0; i < n-1; i++)
+        {
+            // Find the minimum element in unsorted array
+            int min_idx = i;
+            for (int j = i+1; j < n; j++)
+                if (array[j] < array[min_idx])
+                    min_idx = j;
+
+            // Swap the found minimum element with the first
+            // element
+            int temp = array[min_idx];
+            array[min_idx] = array[i];
+            array[i] = temp;
         }
     }
 
