8e7dae3451e60fe99d933304c82380d99ef29adf
Robin Kamboj <dev.robinkamboj@gmail.com>
2018-12-04 05:25:59 +0530

    quick sort

--- src/robillo/sorting/QuickSort.java
+++ src/robillo/sorting/QuickSort.java
@@ -4,21 +4,61 @@ public class QuickSort {
 
     public static void main(String[] args) {
         int array[] = {64, 34, 25, 12, 22, 11, 90};
-        bubbleSort(array);
+        quickSort(array, 0, array.length - 1);
         System.out.println("Sorted array is:");
         printArray(array);
     }
 
-    private static void bubbleSort(int[] array) {
-        int n = array.length;
-        for(int i=0; i<n-1 ; i++) {         //leaving the last one, which will sort by itself
-            for(int j=0; j<n-i-1; j++) {    //first it ends at second last, then third last, ...
-                //swap
-                int temp = array[j];
-                array[j] = array[i];
-                array[i] = temp;
+    /* The main function that implements QuickSort()
+      arr[] --> Array to be sorted,
+      low  --> Starting index,
+      high  --> Ending index */
+    private static void quickSort(int arr[], int low, int high)
+    {
+        if (low < high)
+        {
+            /* pi is partitioning index, arr[pi] is
+              now at right place */
+            int pi = partition(arr, low, high);
+
+            // Recursively sort elements before
+            // partition and after partition
+            quickSort(arr, low, pi-1);
+            quickSort(arr, pi+1, high);
+        }
+    }
+
+    /* This function takes last element as pivot,
+   places the pivot element at its correct
+   position in sorted array, and places all
+   smaller (smaller than pivot) to left of
+   pivot and all greater elements to right
+   of pivot */
+    private static int partition(int arr[], int low, int high)
+    {
+        int pivot = arr[high];
+        int i = (low-1); // index of smaller element
+        for (int j=low; j<high; j++)
+        {
+            // If current element is smaller than or
+            // equal to pivot
+            if (arr[j] <= pivot)
+            {
+                i++;
+
+                // swap arr[i] and arr[j]
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
             }
         }
+
+        // swap arr[i+1] and arr[high] (or pivot)
+        int temp = arr[i+1];
+        arr[i+1] = arr[high];
+        arr[high] = temp;
+
+        return i+1;
     }
 
     private static void printArray(int[] array) {
